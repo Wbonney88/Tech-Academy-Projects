c3b958990974b0cbc6853e6087d421cb0aaf3129
Bill <Wbonney88@gmail.com>
2017-06-28 10:44:16 -0700

    C# Item 17

---
+++ C#/Item 17/EpicSpies/EpicSpies/index.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="EpicSpies.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Image ID="spyImage" runat="server" Height="190px" ImageUrl="epic-spies-logo.jpg" />
+            <br />
+            <br />
+            Spy Code Name:&nbsp;
+            <asp:TextBox ID="codeNameTextBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            New Assignment Name:&nbsp;
+            <asp:TextBox ID="assignmentTextBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            End Date of Previous Assignment<asp:Calendar ID="previousAssignCalendar" runat="server"></asp:Calendar>
+            <br />
+            <br />
+            Start Date of New Assignment<asp:Calendar ID="newAssignCalendar" runat="server"></asp:Calendar>
+            <br />
+            Projected End Date of New Assignment<br />
+            <asp:Calendar ID="endAssignCalendar" runat="server"></asp:Calendar>
+            <br />
+            <br />
+            <asp:Button ID="assignButton" runat="server" OnClick="assignButton_Click" Text="Assign Spy" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+            <br />
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ C#/Item 17/EpicSpies/EpicSpies/index.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EpicSpies
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                previousAssignCalendar.SelectedDate = DateTime.Now.Date;
+                newAssignCalendar.SelectedDate = DateTime.Now.Date.AddDays(14);
+                newAssignCalendar.VisibleDate = DateTime.Now.Date.AddDays(14);
+                endAssignCalendar.SelectedDate = DateTime.Now.Date.AddDays(21);
+                endAssignCalendar.VisibleDate = DateTime.Now.Date.AddDays(21);
+            }
+        }
+
+        protected void assignButton_Click(object sender, EventArgs e)
+        {
+            ///To test for at least 2 weeks inbetween assignments.
+            TimeSpan daysBetweenAssignments = newAssignCalendar.SelectedDate.Subtract(previousAssignCalendar.SelectedDate);
+            if (daysBetweenAssignments.TotalDays < 14)
+            {
+                DateTime earliestNewAssign = previousAssignCalendar.SelectedDate.AddDays(14);
+                newAssignCalendar.SelectedDate = earliestNewAssign;
+                newAssignCalendar.VisibleDate = earliestNewAssign;
+
+                resultLabel.Text = String.Format("Error: You must allow at least two weeks of vacation between the previous assignment and the new assignment."
+                    + " The earliest start date for the new assignment is {0:MM/d/yy}", earliestNewAssign);
+
+                
+
+            }
+            else
+            {
+                ///To get budget cost of assignment.
+                TimeSpan lengthOfAssignment = endAssignCalendar.SelectedDate.Subtract(newAssignCalendar.SelectedDate);
+                double totalCost = lengthOfAssignment.TotalDays * 500;
+                if (lengthOfAssignment.TotalDays > 21)
+                {
+                    totalCost = totalCost + 1000;
+                }
+
+                resultLabel.Text = String.Format("You are authorized to assign {0} to assignment {1}. Total budget: {2:C}", codeNameTextBox.Text, assignmentTextBox.Text, totalCost);
+            }
+        }
+    }
+}
